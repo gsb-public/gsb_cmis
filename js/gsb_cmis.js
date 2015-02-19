170255826a29aec73fbf6851e0adddfa386e8a42
Greg Mercer (GBear) <gmercer@stanford.edu>
2015-02-19 14:11:02 -0800

    WP-4915 Adding authtype setting

--- js/gsb_cmis.js
+++ js/gsb_cmis.js
@@ -4,14 +4,15 @@
 (function($){
   Drupal.behaviors.gsbCMIS = {
     attach: function (context, settings) {
-    window.setTimeout(function () {
-      var nid = settings.alfresco_document.nid;
-      var url = Drupal.settings.basePath + 'gsb-cmis/gsb-cmis-get-alfresco-doc/' + nid;
-      window.location.replace(url);
-    }, 1000);
-    $('.close-window').bind('click', function(context) {
-      window.open('','_self').close();
-    });
+      window.setTimeout(function () {
+        var nid = settings.alfresco_document.nid;
+        var authtype = settings.alfresco_document.authtype;
+        var url = Drupal.settings.basePath + 'gsb-cmis/gsb-cmis-get-alfresco-doc/' + nid + '/' + authtype;
+        window.location.replace(url);
+      }, 1000);
+      $('.close-window').bind('click', function(context) {
+        window.open('','_self').close();
+      });
     }
   };
 })(jQuery);
